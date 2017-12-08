748b511a5e1c71326de49a07d29c1a05782f02be
Chibuikem Amaechi <chibuikem_amaechi@Chibuikems-MacBook-Pro.local>
2017-12-08 11:23:54 -0600

    Modified .main.cpp.swp and main.cpp

--- main.cpp
+++ main.cpp
@@ -47,145 +47,145 @@ bool playerWins(char game[][COLUMN], char player);
 
 int main()
 {
-	char ticTacToe[ROW][COLUMN]= {'1', '2', '3', '4', '5', '6', '7', '8', '9'}, player;
-	int row, column, turn = 0;
-	
-	displayTicTacToeGame(ticTacToe);
+    char ticTacToe[ROW][COLUMN]= {'1', '2', '3', '4', '5', '6', '7', '8', '9'}, player;
+    int row, column, turn = 0;
 
-	do
-	{
-		playerInput(row, column, player, turn);
-		updateTicTacToeGame(ticTacToe, row, column, player);
+    displayTicTacToeGame(ticTacToe);
 
-	}while(!(playerWins(ticTacToe, player)));
+    do
+    {
+        playerInput(row, column, player, turn);
+        updateTicTacToeGame(ticTacToe, row, column, player);
 
-	cout<<"Player "<<player<<" wins!\n";
+    }while(!(playerWins(ticTacToe, player)));
 
-	return 0;
+    cout<<"Player "<<player<<" wins!\n";
+
+    return 0;
 }
 
 void playerInput(int& row, int& column, char& player, int& turn)
 {
-	if (turn == 0)
-	{
-		player = 'X';
-		turn = 1;
-	}
-	else 
-	{
-		player = 'O';
-		turn = 0;
-	}
+    if (turn == 0)
+    {
+        player = 'X';
+        turn = 1;
+    }
+    else 
+    {
+        player = 'O';
+        turn = 0;
+    }
 
-	cout<<"Player "<<player<<", mark your position (in coordinates): ";
-	cin>>row>>column;
-	cout<<"\n\n";
+    cout<<"Player "<<player<<", mark your position (in coordinates): ";
+    cin>>row>>column;
+    cout<<"\n\n";
 }
 
 void updateTicTacToeGame(char game[][COLUMN], int row, int column, char player)
 {
-	game[row][column] = player;
+    game[row][column] = player;
 
-	for (int row = 0; row < ROW; row++)
-	{
-		cout<<setw(37);
+    for (int row = 0; row < ROW; row++)
+    {
+        cout<<setw(37);
 
-		for (int column = 0; column < COLUMN; column++)
-			cout<<game[row][column]<<" ";
-		
-		cout<<"\n\n";
-	}
+        for (int column = 0; column < COLUMN; column++)
+            cout<<game[row][column]<<" ";
+
+        cout<<"\n\n";
+    }
 }
 
 void displayTicTacToeGame(char game[][COLUMN])
 {
-	for (int row = 0; row < ROW; row++)
-	{
-		cout<<setw(37);
+    for (int row = 0; row < ROW; row++)
+    {
+        cout<<setw(37);
 
-		for (int column = 0; column < COLUMN; column++)
-			cout<<game[row][column]<<" ";
-		
-		cout<<"\n\n";
-	}
+        for (int column = 0; column < COLUMN; column++)
+            cout<<game[row][column]<<" ";
+
+        cout<<"\n\n";
+    }
 }
 
 bool playerWins(char game[][COLUMN], char player)
 {
-	return (isHorizontal(game, player) || isVertical(game, player) || isDiagonal(game, player));
+    return (isHorizontal(game, player) || isVertical(game, player) || isDiagonal(game, player));
 }
 
 bool isHorizontal(char game[][COLUMN], char player)
 {
-	int threeInARow = 0;
+    int threeInARow = 0;
 
-	for (int row = 0; row < ROW; row++)
-	{
-		for (int column = 0; column < COLUMN; column++)
-		{
-			if (game[row][column] == player)
-				++threeInARow;
-		}
+    for (int row = 0; row < ROW; row++)
+    {
+        for (int column = 0; column < COLUMN; column++)
+        {
+            if (game[row][column] == player)
+                ++threeInARow;
+        }
 
-		if (threeInARow == 3)
-			return true;
-		else
-			threeInARow = 0;
-	}
+        if (threeInARow == 3)
+            return true;
+        else
+            threeInARow = 0;
+    }
 
-	return false;
+    return false;
 }
 
 bool isVertical(char game[][COLUMN], char player)
 {
-	int threeInARow = 0;
+    int threeInARow = 0;
 
-	for (int column = 0; column < COLUMN; column++)
-	{
-		for (int row = 0; row < ROW; row++)
-		{
-			if (game[row][column] == player)
-				++threeInARow;
-		}
+    for (int column = 0; column < COLUMN; column++)
+    {
+        for (int row = 0; row < ROW; row++)
+        {
+            if (game[row][column] == player)
+                ++threeInARow;
+        }
 
-		if (threeInARow == 3)
-			return true;
-		else
-			threeInARow = 0;
-	}
+        if (threeInARow == 3)
+            return true;
+        else
+            threeInARow = 0;
+    }
 
-	return false;
+    return false;
 }
 
 bool isDiagonal(char game[][COLUMN], char player)
 {
-	int threeInARow = 0;
+    int threeInARow = 0;
 
-	for (int row = 0; row < ROW; row++)
-	{
-		for (int column = 0; column < COLUMN; column++)
-		{
-			if ((row == column) && game[row][column] == player)
-				++threeInARow;
-		}
+    for (int row = 0; row < ROW; row++)
+    {
+        for (int column = 0; column < COLUMN; column++)
+        {
+            if ((row == column) && game[row][column] == player)
+                ++threeInARow;
+        }
 
-		if (threeInARow == 3)
-			return true;
-	}
-	
-	threeInARow = 0;
+        if (threeInARow == 3)
+            return true;
+    }
 
-	for (int row = 0; row < ROW; row++)
-	{
-		for (int column = 0; column < COLUMN; column++)
-		{
-			if ((row + column == COLUMN - 1) && game[row][column] == player)
-				++threeInARow;
-		}
+    threeInARow = 0;
 
-		if (threeInARow == 3)
-			return true;
-	}
+    for (int row = 0; row < ROW; row++)
+    {
+        for (int column = 0; column < COLUMN; column++)
+        {
+            if ((row + column == COLUMN - 1) && game[row][column] == player)
+                ++threeInARow;
+        }
 
-	return false;
+        if (threeInARow == 3)
+            return true;
+    }
+
+    return false;
 }
